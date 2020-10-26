a6c6485c6ea62c5df918e1ac1a04e59da37cdedc
violinalan <alan@tierra.net>
2020-10-26 06:56:16 -0700

    fixed bugs

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-let poke_input = fs.readFile('input.txt');
+let poke_input = "";
+poke_input = fs.readFileSync("input.txt", "utf8");
 
-poke_input = poke_input.split("\n");
-poke_input = poke_input.map(entry => entry.toLowerCase());
+poke_input = poke_input.replace(/(\r)/gm, "");
+let poke_input_arr = poke_input.split('\n');
+poke_input_arr = poke_input_arr.map(entry => entry.toLowerCase());
 
 function read_pokemon_data (name) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
@@ -14,8 +16,8 @@ function read_pokemon_data (name) {
             //for(let nums of data.types) {
             //    data.types[nums].type.name;
             //}
-            console.log(name[0].toUpperCase + name.substring(1) + ": " + data.types.map(element => element.type.name).join(", "))
+            console.log(name[0].toUpperCase() + name.substring(1) + ": " + data.types.map(element => element.type.name).join(", "))
         })
 }
 
-poke_input.forEach(entry => read_pokemon_data(entry));
+poke_input_arr.forEach(entry => read_pokemon_data(entry));
